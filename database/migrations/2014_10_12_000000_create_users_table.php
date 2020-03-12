0e559d04de510256c2d28e783743fee93ae5f1d1
Zainab-dev-web <zainabfahem96@gmail.com>
2020-03-12 14:56:58 +0100

    retire mot de passe

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,6 @@ class CreateUsersTable extends Migration
             $table->integer('age');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             
             $table->timestamps();
         });
